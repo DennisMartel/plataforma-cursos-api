178feffb2bf16b0e487841721f6b656d407ed7c3
DennisMartel <marteldennis05@gmail.com>
2024-02-11 17:09:07 -0600

    separacion de textos para footer y header

--- app/Http/Controllers/Resources/Common.php
+++ app/Http/Controllers/Resources/Common.php
@@ -5,19 +5,26 @@ namespace App\Http\Controllers\Resources;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
+use stdClass;
 
 class Common extends Controller
 {
   public function __invoke()
   {
     $language = request("language");
+    $queryParamHeader = request("header");
+    $queryParamFooter = request("footer");
     $staticDir = "resources/langs/$language/common.json";
     $jsonFileExist = Storage::disk("public")->exists($staticDir);
 
     if ($jsonFileExist) {
-      $jsonFile = json_decode(Storage::disk("public")->get($staticDir));
+      $jsonFile = json_decode(Storage::disk("public")->get($staticDir), true);
 
-      return response()->json($jsonFile, Response::HTTP_OK)
+      $jsonObject = new stdClass();
+      $queryParamHeader === "true" && $jsonObject->header = $jsonFile["header"];
+      $queryParamFooter === "true" && $jsonObject->footer = $jsonFile["footer"];
+
+      return response()->json($jsonObject, Response::HTTP_OK)
         ->header('Access-Control-Allow-Origin', '*');
     }
 
