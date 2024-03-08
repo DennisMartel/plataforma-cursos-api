dd1f4a4ce1aa6185a090e11f6cf8751b2a5969db
DennisMartel <marteldennis05@gmail.com>
2024-03-07 21:01:46 -0600

    agregando url dummy al factory lesson

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -9,16 +9,24 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class LessonFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            "title" => $this->faker->sentence(),
-            "url" => "https://youtu.be/kTe1XZfYq9M?si=_37fAfaszrMhZGfH",
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    $urls = [
+      "https://youtu.be/FKsoF3htA6s?si=GocC0_5ZON2oUX3w",
+      "https://youtu.be/ECDgNM9xEYQ?si=tkxqWo-1zN7MSNM7",
+      "https://youtu.be/kTe1XZfYq9M?si=_37fAfaszrMhZGfH",
+      "https://youtu.be/WAJKSPJnIKo?si=nNcHG8m_aITgV_Vv",
+      "https://youtu.be/j7vXHVtHJ1E?si=pBYSN9F6vAUINmAq"
+    ];
+
+    return [
+      "title" => $this->faker->sentence(),
+      "url" => $this->faker->randomElement($urls),
+    ];
+  }
 }
